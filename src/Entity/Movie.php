ee74a9ebc1b4711339b17843ec77e3df81af82a7
Maeckeul <cmaeckeul@gmail.com>
2020-08-19 13:03:13 +0200

    :construction: OneToMany Category/Movie

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -26,6 +26,11 @@ class Movie {
      */
     private $releaseDate;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     */
+    private $category;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -51,6 +56,18 @@ class Movie {
     public function setReleaseDate(\DateTimeInterface $releaseDate): self
     {
         $this->releaseDate = $releaseDate;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
 
         return $this;
     }
