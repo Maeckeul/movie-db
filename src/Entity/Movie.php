4df89131e7ecd4a21caca868e227bb5257874312
Maeckeul <cmaeckeul@gmail.com>
2020-08-20 18:53:20 +0200

    :alembic: query custom to delete request DB

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -2,12 +2,13 @@
 
 namespace App\Entity;
 
+use App\Repository\MovieRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
  */
 class Movie
 {
---
+++ src/Repository/MovieRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Movie;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Category|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Category|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Category[]    findAll()
+ * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MovieRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Movie::class);
+    }
+
+    public function findWithActors($id) {
+
+        
+    }
+}
