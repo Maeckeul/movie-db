ef6c73ac57005540eb06d0fdc09041e7f7cbe133
Maeckeul <cmaeckeul@gmail.com>
2020-08-23 18:44:21 +0200

    :construction: fixtures created Persons & Categories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Person;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,9 +11,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $category1 = new Category();
-        $category1->setLabel("Science-Fiction");
-        $manager->persist($category1);
+        $categories = ["Science-Fiction", "Action", "Comédie"];
+        foreach($categories as $categoryName) {
+
+            $category = new Category();
+            $category->setLabel($categoryName);
+            $manager->persist($category);
+        }
+
+        for($i=0; $i < 200; $i++) {
+            $person = new Person();
+            $person->setName("Person" . $i);
+            $person->setBirthDate(new \DateTime("1972-01-01"));
+            $manager->persist($person);
+        }
+
         $manager->flush();
     }
 }
