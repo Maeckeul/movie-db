86a5294f21d91ac95a4caaf382037ac55264ec3c
Maeckeul <cmaeckeul@gmail.com>
2020-09-02 10:18:13 +0200

    :construction: dependancies injections

--- src/Command/MovieSlugifyAllCommand.php
+++ src/Command/MovieSlugifyAllCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use App\Service\Slugger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,27 +14,34 @@ class MovieSlugifyAllCommand extends Command
 {
     protected static $defaultName = 'app:movie:slugify-all';
 
+    private $slugger;
+
+    public function __construct(Slugger $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     protected function configure()
     {
         $this
-            ->setDescription('Add a short description for your command')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->setDescription('Calcule et met à jour le slug de tous les films en base de données')
+            // ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            // ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        // $arg1 = $input->getArgument('arg1');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        // if ($arg1) {
+        //     $io->note(sprintf('You passed an argument: %s', $arg1));
+        // }
 
-        if ($input->getOption('option1')) {
+        // if ($input->getOption('option1')) {
             // ...
-        }
+        // }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
