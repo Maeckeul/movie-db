d79a0c5a95ef2c2f2185efa24736ac82ae1f162b
Maeckeul <cmaeckeul@gmail.com>
2020-09-03 18:08:50 +0200

    :alembic: event created

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,12 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class UserType extends AbstractType
 {
@@ -33,6 +36,28 @@ class UserType extends AbstractType
                 'mapped' => false, 
                 "required" => false
             ])
+            ->addEventListener(FormEvents::POST_SET_DATA, function(FormEvent $event) {
+                $form = $event->getForm();
+                $user = $event->getData();
+
+                if($user->getId() == null) {
+                    $form
+                        ->remove('password')
+                        ->add('password', RepeatedType::class, [
+                            'type' => PasswordType::class,
+                            'invalid_message' => 'Les deux mots de passe doivent être identiques',
+                            'first_options' => ['label' => 'Nouveau mot de passe'],
+                            'second_options' => ['label' => 'Répétez le nouveau mot de passe'],
+                            'mapped' => false, 
+                            "required" => true
+                        ])
+                        ->add('agreeTerms', CheckboxType::class, [
+                            'mapped' => false,
+                            'label' => 'J\'accepte les Conditions Générales d\'Utilisation'
+                        ])
+                    ;
+                }
+            })
         ;
     }
 
