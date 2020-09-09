cd35667b661133c3b5d41f0afcd5e90bf9e5fcad
Maeckeul <cmaeckeul@gmail.com>
2020-09-09 12:07:52 +0200

    :construction: unit-test for Slugger and ImageUploader

---
+++ tests/Service/ImageUploaderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\ImageUploader;
+use PHPUnit\Framework\TestCase;
+
+class ImageUploaderTest extends TestCase
+{
+    public function testGetRandomFileName()
+    {
+        $imageUploader = new ImageUploader();
+
+        $randomFileName = $imageUploader->getRandomFileName('jpg');
+
+        $this->assertIsString($randomFileName);
+        $this->assertEquals(16, strlen($randomFileName));
+    }
+}
--- tests/Service/SluggerTest.php
+++ tests/Service/SluggerTest.php
@@ -2,12 +2,18 @@
 
 namespace App\Tests\Service;
 
+use App\Service\Slugger;
 use PHPUnit\Framework\TestCase;
 
 class SluggerTest extends TestCase
 {
-    public function testSomething()
+    public function testSlugify()
     {
-        $this->assertTrue(true);
+        $slugger = new Slugger();
+
+        $this->assertEquals('seven', $slugger->slugify('Seven'));
+        $this->assertEquals('rrrrrrr-', $slugger->slugify('RRRrrrr !!!'));
+        $this->assertEquals('2012', $slugger->slugify('2012'));
+        $this->assertEquals('star-wars-episode-iii-la-revanche-des-sith', $slugger->slugify('Star Wars: Episode III - La revanche des Sith'));
     }
 }
